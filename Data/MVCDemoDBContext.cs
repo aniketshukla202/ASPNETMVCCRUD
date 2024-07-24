02096c3c3b9ab4439fc41b91ca74291392a29975
Altaf Ansari <altaf.ansari@cmssl.in>
2024-07-24 18:34:46 +0530

    my first commit

---
+++ Data/MVCDemoDBContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+	public class MVCDemoDBContext : DbContext
+	{
+		public MVCDemoDBContext(DbContextOptions options) : base(options)
+		{
+		}
+
+
+         public DbSet<Employee> Employees { get; set; }
+    }
+}
